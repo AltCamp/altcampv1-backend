3f6499c3f56beeef1979d9846f700f2663bbfb8c
Tobi Balogun <tobi.balo@gmail.com>
2023-01-22 17:04:22 +0100

    new: add "global" error handling function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const errorHandler = require('./utils/errorHandler');
 
 // Signup and login authentication middleware
 require('./src/mentors/passport');
@@ -16,5 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/', indexRouter);
+
+// Error handler
+app.use(errorHandler);
 
 module.exports = app;
