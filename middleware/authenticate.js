4365cb80be0bd97844a08fc042e485a414613826
Skywonda <princesam3600@gmail.com>
2023-02-09 20:21:51 +0100

    fix: jwt middleware fix

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const User = require('../model/student');
+const Account = require('../model/account');
 const JwtStrategy = require('passport-jwt').Strategy;
 
 const config = require('../config/index');
@@ -18,7 +18,7 @@ opts.secretOrKey = config.jwt.secret;
 
 exports.jwtPassport = passport.use(
   new JwtStrategy(opts, (payload, done) => {
-    User.findById(payload.id, (err, user) => {
+    Account.findById(payload.id, (err, user) => {
       if (err) {
         return done(err, false);
       } else if (user) {
