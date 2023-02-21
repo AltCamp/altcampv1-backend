ec723049ba52296027094cf80bf1f06cd61d94af
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-02-21 10:42:45 +0100

    refactor: change method for updating password in db

--- src/student/studentController.js
+++ src/student/studentController.js
@@ -1,6 +1,7 @@
 const { ACCOUNT_TYPES } = require('../../constant');
 const Account = require('../../model/account');
 const { NotFoundError } = require('../../utils/customError');
+const { omit } = require('lodash');
 
 async function getStudents(req, res) {
   const students = await Account.find({
@@ -33,14 +34,20 @@ async function updateStudent(req, res) {
 
 async function changeStudentPassword(req, res) {
   const { password } = req.body;
-  const student = await Account.findByIdAndUpdate(
-    req.user.id,
-    { password },
-    { new: true }
-  );
+
+  let student = await Account.findById(req.user.id);
   if (!student) {
     throw new NotFoundError('Student not found!');
   }
+
+  // save password from request
+  student.password = password;
+  await student.save();
+
+  // prepare response data
+  omit(student.toObject(), ['password']);
+  student = omit(student.toObject(), ['password']);
+
   res.json(student);
 }
 
