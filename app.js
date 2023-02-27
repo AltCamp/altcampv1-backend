a877cf5070985037ffb8c0d1691d58caf55c41f6
Munir Abdullahi <abdullahimunir3@gmail.com>
2023-02-27 12:28:20 +0100

    docs: adds documentation for mentor routes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const YAML = require('yamljs');
+const swaggerUI = require('swagger-ui-express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { errorHandler } = require('./utils/errorHandler');
 require('express-async-errors');
 
+const swaggerJsDocs = YAML.load('./api-mentor.yaml');
+
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use('/api-docs-mentor', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
