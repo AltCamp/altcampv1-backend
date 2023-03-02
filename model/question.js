ddbe7e87dad15b859203bc194cc9f41035a2c372
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-03-02 08:40:48 +0100

    chore(db): update question model with
    - timestamps,
    - unique titles and
    - additional support for upvotes and downvotes

--- model/question.js
+++ model/question.js
@@ -1,32 +1,50 @@
 const mongoose = require('mongoose');
 
-const questionSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const questionSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    upvotes: {
+      type: Number,
+      default: 0,
+    },
+    upvotedBy: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'Account',
+      },
+    ],
+    downvotes: {
+      type: Number,
+      default: 0,
+    },
+    downvotedBy: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'Account',
+      },
+    ],
+    author: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Account',
+      required: true,
+    },
+    answer: {
+      type: [mongoose.Types.ObjectId],
+      ref: 'Answer',
+    },
   },
-  body: {
-    type: String,
-    required: true,
-  },
-  upvotes: {
-    type: Number,
-    default: 0,
-  },
-  downvotes: {
-    type: Number,
-    default: 0,
-  },
-  author: {
-    type: mongoose.Types.ObjectId,
-    ref: 'Account',
-    required: true,
-  },
-  answer: {
-    type: [mongoose.Types.ObjectId],
-    ref: 'Answer',
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Question = mongoose.model('Question', questionSchema);
 
