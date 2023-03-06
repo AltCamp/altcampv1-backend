5c3d8efc3e1437a655e8c24588b99d67b4ef1f46
Peter-TMK <tundepeter01@gmail.com>
2023-03-06 14:11:55 +0100

    docs: implemented documentation for route authentication

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const YAML = require('yamljs');
 const swaggerUI = require('swagger-ui-express');
 const cookieParser = require('cookie-parser');
@@ -6,13 +7,18 @@ const logger = require('morgan');
 const { errorHandler } = require('./utils/errorHandler');
 require('express-async-errors');
 
+const swaggerAuthDocs = YAML.load('./apiAuth.yaml');
 const swaggerJsDocs = YAML.load('./api-mentor.yaml');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
 
+app.use('/apiAuthDocs', swaggerUI.serve, swaggerUI.setup(swaggerAuthDocs));
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
+
+app.use(cors())
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
