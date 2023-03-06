f29809ba73a93a2e92a95539706031e50753274e
Nonso Andrew <nonsoandrew1@gmail.com>
2023-03-06 16:51:24 +0100

    docs: implemented student route documentation

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { errorHandler } = require('./utils/errorHandler');
 require('express-async-errors');
-
-const swaggerJsDocs = YAML.load('./api-mentor.yaml');
+var cors = require('cors');
+const swaggerJsDocs = YAML.load('./api-docs.yaml');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
-
+app.use(cors());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDocs));
 app.use(logger('dev'));
 app.use(express.json());
