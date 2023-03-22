99bea28e0a919082e2029a812a9c6d09d4ba3fc1
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-03-22 09:47:22 +0100

    fix: whitelist addresses for cors

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const { errorHandler } = require('./utils/errorHandler');
+const cors = require('cors');
 require('express-async-errors');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://stdybdyv1.netlify.app',
+    ],
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
