7c2b36cf44ee2eed60d8f7e4b3b6fc080bd31988
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-04-04 10:10:57 +0100

    new: add validators to question routes

--- src/questions/index.js
+++ src/questions/index.js
@@ -1,20 +1,24 @@
 const router = require('express').Router();
 const questions = require('./questions.controller');
 const { verifyUser } = require('../../middleware/authenticate');
-const {
-  updateQuestionValidator,
-  createQuestionValidator,
-} = require('./questions.validator');
+const validatorMiddleware = require('../../middleware/validator');
 
+router.use(verifyUser);
 router
   .route('/')
   .get(questions.getAllQuestions)
-  .post(verifyUser, createQuestionValidator, questions.createQuestion);
+  .post(
+    validatorMiddleware('createQuestionValidator'),
+    questions.createQuestion
+  );
 
 router
   .route('/:id')
   .get(questions.getQuestion)
-  .patch(verifyUser, updateQuestionValidator, questions.updateQuestion)
+  .patch(
+    validatorMiddleware('updateQuestionValidator'),
+    questions.updateQuestion
+  )
   .delete(verifyUser, questions.deleteQuestion);
 
 module.exports = router;
--- src/questions/questions.validator.js
+++ src/questions/questions.validator.js
@@ -1,30 +1,6 @@
 const Joi = require('joi');
 
-const validate = async (validator, payload) => {
-  await validator.validateAsync(payload, { abortEarly: false });
-};
-
-const updateQuestionValidator = async (req, _res, next) => {
-  const payload = req.body;
-  try {
-    await validate(questionUpdate, payload);
-    next();
-  } catch (err) {
-    next(err);
-  }
-};
-
-const createQuestionValidator = async (req, _res, next) => {
-  const payload = req.body;
-  try {
-    await validate(question, payload);
-    next();
-  } catch (err) {
-    next(err);
-  }
-};
-
-const question = Joi.object({
+const createQuestionValidator = Joi.object({
   title: Joi.string().required().min(16).max(100).messages({
     'string.empty': 'Title is required',
     'any.required': 'Title is required',
@@ -35,7 +11,7 @@ const question = Joi.object({
   }),
 });
 
-const questionUpdate = Joi.object({
+const updateQuestionValidator = Joi.object({
   title: Joi.string().optional().min(16).max(100),
   body: Joi.string().optional().min(16),
 });
