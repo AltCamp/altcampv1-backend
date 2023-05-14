9cb350bedf8d4c074e90bca516bde875c0602ac0
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-05-14 10:43:56 +0100

    new: mount answers router

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const studentRouter = require('../src/students');
 const mentorRouter = require('../src/mentors');
 const apiDocs = require('../src/docs');
 const questionsRouter = require('../src/questions');
+const answersRouter = require('../src/answers');
 
 router.use('/auth', authRouter);
 router.use('/students', studentRouter);
 router.use('/mentors', mentorRouter);
 router.use('/questions', questionsRouter);
+router.use('/answers', answersRouter);
 router.use('/api-docs', apiDocs);
 
 module.exports = router;
