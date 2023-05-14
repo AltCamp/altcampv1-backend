ec2b0994ff7662b0165cc4149c2b92aba856b208
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-05-14 10:11:09 +0100

    refactor: update "Students Module"

--- src/students/studentsController.js
+++ src/students/studentsController.js
@@ -1,18 +1,17 @@
 const { ACCOUNT_TYPES, RESPONSE_MESSAGE } = require('../../constant');
-const Account = require('../../model/account');
+const studentsService = require('./studentsService');
 const { NotFoundError } = require('../../utils/customError');
-const { omit } = require('lodash');
 const responseHandler = require('../../utils/responseHandler');
 
 async function getStudents(req, res) {
-  const students = await Account.find({
-    accountType: ACCOUNT_TYPES.STUDENT,
-  }).populate('owner');
-  res.json(students);
+  const students = await studentsService.getStudents();
+
+  new responseHandler(res, students, 200, RESPONSE_MESSAGE.SUCCESS);
 }
 
 async function getSingleStudent(req, res) {
-  const student = await Account.findById(req.params.id).populate('owner');
+  const { id } = req.params;
+  const student = await studentsService.getSingleStudent(id);
 
   if (!student || student.accountType !== ACCOUNT_TYPES.STUDENT) {
     throw new NotFoundError('Student not found!');
@@ -22,12 +21,11 @@ async function getSingleStudent(req, res) {
 }
 
 async function updateStudent(req, res) {
-  const { firstname, lastname } = req.body;
-  const student = await Account.findByIdAndUpdate(
-    req.user.id,
-    { firstname, lastname },
-    { new: true }
-  );
+  const payload = { ...req.body };
+  const student = await studentsService.updateStudent({
+    id: req.user.id,
+    payload,
+  });
   if (!student) {
     throw new NotFoundError('Student not found!');
   }
@@ -37,20 +35,15 @@ async function updateStudent(req, res) {
 async function changeStudentPassword(req, res) {
   const { password } = req.body;
 
-  let student = await Account.findById(req.user.id);
-  if (!student) {
+  const updatedStudent = await studentsService.changeStudentPassword({
+    id: req.user.id,
+    password,
+  });
+  if (!updatedStudent) {
     throw new NotFoundError('Student not found!');
   }
 
-  // save password from request
-  student.password = password;
-  await student.save();
-
-  // prepare response data
-  omit(student.toObject(), ['password']);
-  student = omit(student.toObject(), ['password']);
-
-  new responseHandler(res, student, 200, RESPONSE_MESSAGE.SUCCESS);
+  new responseHandler(res, updatedStudent, 200, RESPONSE_MESSAGE.SUCCESS);
 }
 
 module.exports = {
