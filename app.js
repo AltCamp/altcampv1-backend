ac41ba0c1a620a854a56c2e626d3b68b58a32adc
Peter-TMK <tundepeter01@gmail.com>
2023-05-18 14:38:25 +0100

    feat: added rate limiting to control number of request from an IP

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const rateLimiter = require('express-rate-limit');
 const { errorHandler } = require('./utils/errorHandler');
 const cors = require('cors');
 require('express-async-errors');
 
 const indexRouter = require('./routes/index');
 
+const limiter = rateLimiter({
+  windowMs: 10 * 60 * 1000, // 10 minutes window
+  max: 50, // Limit each IP to 50 requests per `window` (here, per 10 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const app = express();
+
+// applying rate limiting middleware to all requests
+app.use(limiter);
+
 app.use(
   cors({
     origin: [
@@ -18,6 +30,7 @@ app.use(
     ],
   })
 );
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
