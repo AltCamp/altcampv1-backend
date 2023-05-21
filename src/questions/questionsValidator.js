57156dd9c1b5fb270d8327c3d2d045eb6fdb02fb
Munir Abdullahi <abdullahimunir3@gmail.com>
2023-05-21 10:51:32 +0200

    refactor: removed min() validation for questions

--- src/questions/questionsValidator.js
+++ src/questions/questionsValidator.js
@@ -1,19 +1,19 @@
 const Joi = require('joi');
 
 const createQuestionValidator = Joi.object({
-  title: Joi.string().required().min(16).max(100).messages({
+  title: Joi.string().required().max(100).messages({
     'string.empty': 'Title is required',
     'any.required': 'Title is required',
   }),
-  body: Joi.string().required().min(128).messages({
+  body: Joi.string().required().messages({
     'string.empty': 'Body is required',
     'any.required': 'Body is required',
   }),
 });
 
 const updateQuestionValidator = Joi.object({
-  title: Joi.string().optional().min(16).max(100),
-  body: Joi.string().optional().min(16),
+  title: Joi.string().optional().max(100),
+  body: Joi.string().optional(),
 });
 
 module.exports = {
