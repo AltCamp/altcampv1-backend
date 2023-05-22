41e84be504f0b07438152785a6893a5d3fc99842
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-05-22 15:50:34 +0100

    test: test rate limit for login route

--- middleware/__tests__/rateLimit.test.js
+++ middleware/__tests__/rateLimit.test.js
@@ -1,5 +1,6 @@
 const app = require('../../app');
 const supertest = require('supertest');
+const request = supertest(app);
 const helper = require('../../test/testHelper');
 const { dbConnect, dbCleanUP, dbDisconnect } = require('../../test/test.db');
 
@@ -17,22 +18,21 @@ beforeAll(async () => {
   await Promise.all(entities);
 });
 
-// Defining route handlers
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!');
-});
-
-const request = supertest(app);
-
 describe('Rate Limit Test', () => {
   // Increase the timeout for this test to 10 seconds (10000 milliseconds)
   jest.setTimeout(10000);
+
+  const user = helper.accountsAsJson[4];
+  const url = '/auth/login';
+  const data = { email: user.email, password: user.password };
 
   it('should pass when rate limit is not exceeded', async () => {
     // Make multiple requests within the rate limit
     const numRequests = 50;
     const responses = await Promise.all(
-      Array.from({ length: numRequests }).map(() => request.get('/'))
+      Array.from({ length: numRequests }).map(() =>
+        request.post(url).send(data)
+      )
     );
 
     // Assert that all responses have a status code of 200
@@ -40,22 +40,24 @@ describe('Rate Limit Test', () => {
       expect(response.status).toBe(200);
     });
   });
-});
-
-describe('Rate Limit Test', () => {
-  // Increase the timeout for this test to 10 seconds (10000 milliseconds)
-  jest.setTimeout(10000);
 
   it('should fail when rate limit is exceeded', async () => {
     // Make multiple requests within a short time frame to exceed the rate limit
     const numRequests = 51;
     const responses = await Promise.all(
-      Array.from({ length: numRequests }).map(() => request.get('/'))
+      Array.from({ length: numRequests }).map(() =>
+        request.post(url).send(data)
+      )
     );
 
     // Assert that the last response has a status code indicating the rate limit has been exceeded
     const lastResponse = responses[responses.length - 1];
     expect(lastResponse.status).toBe(429);
+    expect(lastResponse.body).toHaveProperty(
+      'message',
+      'Too many requests, please try again later'
+    );
+    expect(lastResponse.body).toHaveProperty('statusCode', lastResponse.status);
   });
 });
 
