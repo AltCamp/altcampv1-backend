c6d682db16f8bf0dd43b59a4081e74b5427feb91
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-05-25 08:56:31 +0100

    refactor: use /accounts route

--- src/accounts/index.js
+++ src/accounts/index.js
@@ -1,41 +1,37 @@
 const router = require('express').Router();
 const { verifyUser } = require('../../middleware/authenticate');
-const { uploadProfilePicture } = require('./accountsController');
+const {
+  getAccount,
+  getAccounts,
+  updateAccount,
+  uploadProfilePicture,
+} = require('./accountsController');
 const multer = require('multer');
+const {
+  getAccountsValidator,
+  profileBioValidator,
+  profileValidator,
+} = require('./accountsValidator');
+const validator = require('express-joi-validation').createValidator({
+  passError: true,
+});
+const validatorMiddleware = require('../../middleware/validator');
 
 const upload = multer({ dest: 'src/accounts/tmp/uploads/' });
 
 router
   .route('/')
-  .get((req, res) => {
-    res.json({
-      message: 'Here are all accounts',
-    });
-  })
-  .put(verifyUser, (req, res) => {
-    res.json({
-      message: 'Update this account',
-    });
-  });
+  .get(validator.query(getAccountsValidator), getAccounts)
+  .put(verifyUser, validatorMiddleware(profileValidator), updateAccount);
 
-router.route('/bio').put(verifyUser, (req, res) => {
-  res.json({
-    message: 'Update bio',
-  });
-});
-router.route('/password').put(verifyUser, (req, res) => {
-  res.json({
-    message: 'Password update',
-  });
-});
+router
+  .route('/bio')
+  .put(verifyUser, validatorMiddleware(profileBioValidator), updateAccount);
+
 router
   .route('/upload-profile-picture')
   .put(verifyUser, upload.single('profilePicture'), uploadProfilePicture);
 
-router.route('/:id').get((req, res) => {
-  res.json({
-    message: 'Here is a single account',
-  });
-});
+router.route('/:id').get(getAccount);
 
 module.exports = router;
