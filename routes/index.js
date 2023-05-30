4dcb8e088e7339317c3a11250d3cd3bbbe3946d7
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-05-30 22:19:52 +0100

    chore: remove deprecated routes

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authRouter = require('../src/auth');
 const accountRouter = require('../src/accounts');
-const studentRouter = require('../src/students');
-const mentorRouter = require('../src/mentors');
 const apiDocs = require('../src/docs');
 const questionsRouter = require('../src/questions');
 const answersRouter = require('../src/answers');
 
 router.use('/auth', authRouter);
 router.use('/accounts', accountRouter);
-router.use('/students', studentRouter);
-router.use('/mentors', mentorRouter);
 router.use('/questions', questionsRouter);
 router.use('/answers', answersRouter);
 router.use('/api-docs', apiDocs);
