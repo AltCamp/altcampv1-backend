ce33a9a5356d0aea046fa0386728fc7106202d36
Oluwatobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-06-08 07:17:40 +0100

    test: setup tests for Bookmarks module

--- src/bookmarks/__tests__/bookmarks.test.js
+++ src/bookmarks/__tests__/bookmarks.test.js
@@ -4,6 +4,9 @@ const supertest = require('supertest');
 const api = supertest(app);
 const helper = require('../../../test/testHelper');
 
+const url = '/bookmarks';
+const postType = 'Post';
+const title = 'This is an intriguing thing';
 let token;
 
 beforeAll(async () => {
@@ -11,19 +14,16 @@ beforeAll(async () => {
   await dbConnect();
 
   // get 'dummy' entities from helper
-  const { accounts, students, mentors, questions } = helper;
+  const { accounts, bookmarks, students, mentors, posts } = helper;
 
   // data structure to hold 'dummy' entities to be created
-  const entities = accounts.concat(students, mentors, questions);
+  const entities = accounts.concat(bookmarks, students, mentors, posts);
 
   // create entities in database
   await Promise.all(entities);
 });
 
 describe('Creating a bookmark', () => {
-  const url = '/bookmarks';
-  const postType = 'Post';
-
   // get a post
   test('fails if a user is not logged in', async () => {
     const postsInDb = await helper.postsInDb();
@@ -33,6 +33,7 @@ describe('Creating a bookmark', () => {
     await api
       .post(url)
       .send({
+        title,
         postType,
         postId: _id,
       })
@@ -51,6 +52,7 @@ describe('Creating a bookmark', () => {
       .post(url)
       .set('Authorization', `Bearer ${token}`)
       .send({
+        title,
         postType,
         postId: _id,
       })
@@ -59,6 +61,7 @@ describe('Creating a bookmark', () => {
 
     // check response for specific properties
     expect(response.body.data).toHaveProperty('_id');
+    expect(response.body.data).toHaveProperty('title', title);
     expect(response.body.data).toHaveProperty('isRead', false);
     expect(response.body.data.owner).toEqual(
       expect.objectContaining({
@@ -70,11 +73,174 @@ describe('Creating a bookmark', () => {
     expect(response.body.data).toHaveProperty('postType', postType);
     expect(response.body.data.post).toEqual(
       expect.objectContaining({
-        _id,
-        content,
+        _id: _id.toString(),
+        content: content,
       })
     );
-    expect(response.body.data).toHaveProperty('owner', user._id);
+  });
+});
+
+describe('Fetching bookmarks', () => {
+  test('fails if user is not logged in', async () => {
+    await api.get(url).expect(401);
+  });
+
+  test('only returns bookmarks of logged in user', async () => {
+    const user = helper.accountsAsJson[2];
+    await login(user);
+
+    const response = await api
+      .get(url)
+      .set('Authorization', `Bearer ${token}`)
+      .expect('Content-Type', /application\/json/);
+
+    let owner = response.body.data.map(({ owner }) => owner._id.toString());
+    owner = new Set(owner);
+    owner = Array.from(owner);
+    expect(owner.length).toBe(1);
+    expect(owner[0]).toBe(user._id);
+  });
+
+  test('by ID returns 404 if logged in user is not author', async () => {
+    const bookmarksInDb = await helper.bookmarksInDb();
+    const { _id } = bookmarksInDb[0];
+
+    const user = helper.accountsAsJson[0];
+    await login(user);
+
+    await api
+      .get(`${url}/${_id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(404);
+  });
+});
+
+describe('Updating a bookmark', () => {
+  test('fails if user is not logged in', async () => {
+    const bookmarksInDb = await helper.bookmarksInDb();
+    const { _id } = bookmarksInDb[0];
+
+    await api
+      .patch(`${url}/${_id}`)
+      .send({
+        isRead: true,
+      })
+      .expect(401);
+  });
+
+  test('fails if logged in user is not owner', async () => {
+    const user = helper.accountsAsJson[4];
+    await login(user);
+
+    const bookmarksInDb = await helper.bookmarksInDb();
+    const { _id } = bookmarksInDb[0];
+
+    await api
+      .patch(`${url}/${_id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Heyyo!',
+        isRead: true,
+      })
+      .expect(401);
+  });
+
+  test('is successful if logged in user is owner', async () => {
+    const bookmarksInDb = await helper.bookmarksInDb();
+    const { _id, owner, post } = bookmarksInDb[0];
+
+    const postsInDb = await helper.postsInDb();
+    const bookmarkedPost = postsInDb.find(
+      ({ _id }) => _id.toString() === post.toString()
+    );
+
+    const user = helper.accountsAsJson.find(
+      ({ _id }) => _id === owner.toString()
+    );
+    await login(user);
+    const title2 = 'Heyyo!';
+
+    const response = await api
+      .patch(`${url}/${_id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: title2,
+        isRead: true,
+      })
+      .expect(200);
+
+    expect(response.body.data).toHaveProperty('_id', _id.toString());
+    expect(response.body.data).toHaveProperty('title', title2);
+    expect(response.body.data).toHaveProperty('isRead', true);
+    expect(response.body.data.owner).toEqual(
+      expect.objectContaining({
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      })
+    );
+    expect(response.body.data).toHaveProperty('postType', postType);
+    expect(response.body.data.post).toEqual(
+      expect.objectContaining({
+        _id: bookmarkedPost._id.toString(),
+        content: bookmarkedPost.content,
+      })
+    );
+  });
+
+  test('fails if request contains invalid properties', async () => {
+    const bookmarksInDb = await helper.bookmarksInDb();
+    const { _id, owner } = bookmarksInDb[0];
+
+    const user = helper.accountsAsJson.find(
+      ({ _id }) => _id === owner.toString()
+    );
+    await login(user);
+
+    await api
+      .patch(`${url}/${_id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        balablue: 'bulaba!',
+      })
+      .expect(422);
+  });
+});
+
+describe('Deleting a bookmark', () => {
+  test('fails if user is not logged in', async () => {
+    const bookmarksInDb = await helper.bookmarksInDb();
+    const { _id } = bookmarksInDb[0];
+
+    await api.delete(`${url}/${_id}`).expect(401);
+  });
+
+  test('fails if logged in user is not owner', async () => {
+    const user = helper.accountsAsJson[4];
+    await login(user);
+
+    const bookmarksInDb = await helper.bookmarksInDb();
+    const { _id } = bookmarksInDb[0];
+
+    await api
+      .delete(`${url}/${_id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(401);
+  });
+
+  test('is successful if user is owner', async () => {
+    const bookmarksInDb = await helper.bookmarksInDb();
+    const { _id, owner } = bookmarksInDb[0];
+
+    const user = helper.accountsAsJson.find(
+      ({ _id }) => _id === owner.toString()
+    );
+    await login(user);
+
+    await api
+      .delete(`${url}/${_id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
   });
 });
 
