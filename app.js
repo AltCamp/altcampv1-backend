bea330811b61860454da51ff433400ea024c8be2
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-06-23 03:16:11 +0100

    fix(server config): increase the maximum request size limit

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(
   })
 );
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
