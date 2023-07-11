58e2969bab4b606ed42bd406326dc0ce99b1b5fe
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-07-11 23:39:37 +0100

    chore: add favicon [skip-ci]

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ const appConfig = (app) => {
   app.use(morgan('combined', { stream: logger.stream }));
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: false }));
+  app.use(express.static('public'));
   app.use(cookieParser());
 
   app.get('/', (req, res) => {
